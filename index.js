18ec09bc3d7cfc044abd4e879703bf226a555315
Juan José Salvador <juanjosalvador@openmailbox.org>
2018-01-23 11:58:55 +0100

    StandardJS fixes

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const express = require('express')
-const path    = require('path')
-const Kitsu   = require('kitsu')
-const utils   = require('./utils/main.js')
-const cors    = require('cors')
+const Kitsu = require('kitsu')
+const utils = require('./utils/main.js')
+const cors = require('cors')
 
 const app = express()
 const api = new Kitsu()
@@ -12,15 +11,15 @@ const HOST = '0.0.0.0'
 
 app.use(cors())
 
-app.get('/anime', (req,res) => {
+app.get('/anime', (req, res) => {
   let params = {
     'filter': {
-        'status': 'current',
-        'subtype': 'TV',
+      'status': ['current'],
+      'subtype': 'TV'
     },
     'sort': 'popularityRank',
     'page': {
-        'limit': 20
+      'limit': 20
     }
   }
   api.get('anime', params).then(
@@ -31,14 +30,48 @@ app.get('/anime', (req,res) => {
   )
 })
 
-app.get('/upcoming', (req,res) => {
-  res.send('Upcoming anime')
+app.get('/upcoming', (req, res) => {
+  let params = {
+    'filter': {
+      'status': 'upcoming'
+    },
+    'sort': 'popularityRank',
+    'page': {
+      'limit': 18
+    }
+  }
+
+  api.get('anime', params).then(
+    (response) => {
+      res.send(response.data)
+    },
+    (error) => res.send(error)
+  )
 })
 
-app.post('/export', (req,res) => {
+app.get('/upcoming/:season', (req, res) => {
+  let params = {
+    'filter': {
+      'status': 'upcoming',
+      'season': req.params.season
+    },
+    'sort': 'popularityRank',
+    'page': {
+      'limit': 18
+    }
+  }
+
+  api.get('anime', params).then(
+    (response) => {
+      res.send(response.data)
+    },
+    (error) => res.send(error)
+  )
+})
+
+app.post('/export', (req, res) => {
   let response = api.get('anime')
   res.send(response)
 })
-
 
 app.listen(PORT, HOST)
--- utils/main.js
+++ utils/main.js
@@ -1,28 +1,32 @@
 const moment = require('moment')
 
-exports.assignWeek = (anime_list) => {
-    const weekMap = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+const assignWeek = (animeList) => {
+  const weekMap = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
-    let animeWeek = {
-        'monday': [],
-        'tuesday': [],
-        'wednesday': [],
-        'thursday': [],
-        'friday': [],
-        'saturday': [],
-        'sunday': []
+  let animeWeek = {
+    'monday': [],
+    'tuesday': [],
+    'wednesday': [],
+    'thursday': [],
+    'friday': [],
+    'saturday': [],
+    'sunday': []
+  }
+
+  let weekEnds = moment().endOf('week').add(1, 'd')
+
+  animeList.map((anime) => {
+    let day = moment(anime.startDate).day()
+    day !== 0 ? day -= 1 : day = 6
+
+    let dayWeek = weekMap[day]
+
+    if ((anime.status === 'upcoming' && new Date(anime.startDate) < weekEnds) || (anime.status === 'current')) {
+      animeWeek[dayWeek].push(anime)
     }
+  })
 
-    anime_list.map((anime) => {
-        let day = moment(anime.startDate).day()
-        day != 0 ? day -= 1 : day = 6
-
-        let dayWeek = weekMap[day]
-
-        animeWeek[dayWeek].push(anime)
-    })
-
-    return animeWeek
+  return animeWeek
 }
 
-module.exports
+module.exports = assignWeek
